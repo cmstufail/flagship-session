3849da987f38ffc2329c644e44453d503ae3ac17
cmstufail <cmstufail@gmail.com>
2025-04-19 01:05:48 +0600

    installed  tailwindcss, DaisyUI and react router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component:  App 
+  },
+  {
+    path: '/about',
+    element: <p>This is an about page</p>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}>
+    
+    </RouterProvider>
   </StrictMode>,
 )
